4d865679a4b30f4bb35a2c757e39e21582893c9e
38 <xx@38.com>
2017-12-04 05:03:55 +0800

    more complex tasks

--- grunt_hello/Gruntfile.js
+++ grunt_hello/Gruntfile.js
@@ -1,4 +1,43 @@
 
 module.exports = function(grunt_name) {
-	console.info('enter grunt function')
+	console.info('enter grunt function');
+	function default_function(){
+		console.info('enter default function');
+	}
+	grunt_name.registerTask('default', default_function);
+	grunt_name.registerTask('world', world);
+
+	function world() {
+		console.info('from world function');
+	}
+
+	class Hello {
+		sayHello(name) {
+			console.info(new Date(), ' hello ', name );
+		}
+	}
+
+	var hmanager = new Hello();
+	hmanager.sayHello('object created using class syntax');
+
+	grunt_name.registerTask('cat', hello_cat);
+	function ooooops() {
+		console.info('ooooops ');
+	}
+	function hello_cat() {
+		console.info('from cat');
+		ooooops();
+		h = new Hello();
+		h.sayHello('cat');
+	}
+	grunt_name.registerTask('dog', function() {
+		console.info('from dog inner function');
+	});
+
+	grunt_name.registerTask('animal', ['dog', 'cat']);
+	grunt_name.registerTask('animal2', ['cat', 'dog']);
+
+
+
+
 }
